bd7faafd4478be0a0d6e2859367e47b40aee8afa
SHOTA Nishimu <nisimuras@kyoto-ryoyo.ed.jp>
2021-03-19 14:13:56 +0900

    1st commit

--- flashcard.py
+++ flashcard.py
@@ -19,7 +19,7 @@ f2.close()
 
 values = re.findall(r'[^a-z\n]+', d2)
 
-word_dict = dict(zip(keys,values))
+word_dict = dict(zip(keys, values))
 
 st.title('古文単語練習')
 
@@ -27,29 +27,24 @@ st.title('古文単語練習')
 ### 次の（　）の単語の意味を答えなさい。
 
 """
-question_num = 1
-for q in range(question_num):
-
-    random_index = np.random.randint(
-        low=0, high=len(keys), size=question_num)
-
-    question_word = keys[random_index[q]]
-    correct_answer = word_dict[question_word]
-    c_answer = keys[random_index[q]]
-
-    st.header(question_word)
-
-    values_copy = values.copy()
-    values_copy.remove(correct_answer)
-    wrong_answers = random.sample(values_copy, 3)
-
-    answer_options = [correct_answer] + wrong_answers
-    random.shuffle(answer_options)
-
-    st.subheader(answer_options)
-
-    expander = st.beta_expander('答えを表示する')
-    expander.header(c_answer)
 
 
+question_word = random.choice(keys)
+correct_answer = word_dict[question_word]
+c_answer=correct_answer
+
+st.header(question_word)
+
+values_copy = values.copy()
+values_copy.remove(correct_answer)
+wrong_answers = random.sample(values_copy, 3)
+
+answer_options = [correct_answer] + wrong_answers
+random.shuffle(answer_options)
+
+st.subheader(answer_options)
+
+expander = st.beta_expander('答えを表示する')
+expander.header(c_answer)
+    
 button = st.button('次の問題を表示する')
